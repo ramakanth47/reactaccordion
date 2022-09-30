54d8b41473aeb77b526bd3dbd3c3a7667817203d
ramakanth47 <105962313+ramakanth47@users.noreply.github.com>
2022-09-30 17:26:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [question, setQuestions]=useState(data);
+  return(
+    <main>
+    <div className='container'>
+      <h3>question and answers about login</h3>
+      <section>
+       {question.map((question)=>{
+        return <SingleQuestion key={question.id} {...question}/>;
+        })}
+      </section>
+    </div>
+    </main>
+  );
+   
+}
+
+export default App;
